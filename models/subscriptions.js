cfe09f54cafcd82d64f496a6f864b206f3636a7c
Paulo McNally <paulomcnally@gmail.com>
2014-12-25 14:16:43 -0600

    added model

---
+++ models/subscriptions.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+exports.setup = function(_mongoose, _db) {
+
+  var fileName = path.basename(__filename, '.js');
+
+  var schema = _mongoose.Schema({
+    email: {
+      type: String,
+      index: {
+        unique: true
+      }
+    },
+    name: String,
+    token: String,
+    active: {
+      type: Boolean,
+      default: false
+    },
+    'created_at': {
+      type: Date,
+      default: Date.now
+    }
+  });
+
+  _db.model(fileName, schema);
+
+  var data = _db.model(fileName);
+
+  return data;
+};
