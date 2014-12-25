ffc122cc4cc2b92e55713ee7adc13ac8c6041fc2
Paulo McNally <paulomcnally@gmail.com>
2014-12-25 15:28:56 -0600

    added controllers method

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,8 @@
 var models = require('../models');
+var randomString = require('randomstring');
 var CronJob = require('cron').CronJob;
 var Lib = require('../lib');
-var lib = new Lib();
+var lib = new Lib(models.subscriptions);
 
 lib.check();
 
@@ -25,9 +26,70 @@ exports.home = function(req, res) {
       version: version
     });
   });
+};
 
+exports.add = function(req, res) {
+  var response = '';
+  var token = randomString.generate();
 
+  var row = new models.subscriptions();
+  row.email = req.body.email;
+  row.token = token;
+  row.save(function(err) {
+    if (err) {
+      response = {
+        status: false,
+        message: err.message
+      };
+    }
+    else {
+      response = {
+        status: true,
+        message: 'You have successfully subscribed! Check email.'
+      };
 
+      var emailSubject = 'Validate you\'re subscription';
+      var emailMessage = 'Please validate you\'re ' +
+      'node.js releases subscription. ' +
+      'http://release.nodenica.com/validate/' + token;
+      lib.sendEmail(req.body.email, emailSubject, emailMessage);
+    }
+    res.send(JSON.stringify(response));
+  });
+};
 
-
+exports.validate = function(req, res) {
+  models.subscriptions.findOne({
+    token: req.params.token
+  }, function(err, data) {
+    if (err) {
+      res.render('validate', {
+        title: 'Error validating email',
+        message: err.message
+      });
+    }
+    else if (data) {
+      data.active = true;
+      data.save(function(err) {
+        if (err) {
+          res.render('validate', {
+            title: 'Error validating email',
+            message: err.message
+          });
+        }
+        else {
+          res.render('validate', {
+            title: 'Error validating email',
+            message: 'You have successfully validated your registration!'
+          });
+        }
+      });
+    }
+    else {
+      res.render('validate', {
+        title: 'Error validating email',
+        message: 'There is no email linked to your validation code!'
+      });
+    }
+  });
 };
