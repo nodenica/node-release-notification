6dd7c1197f545ca31752a5fb6eebaa678eab5a19
Paulo McNally <paulomcnally@gmail.com>
2015-01-14 16:21:49 -0600

    Added link http://releases.nodenica.com/

--- controllers/home.js
+++ controllers/home.js
@@ -51,7 +51,7 @@ exports.add = function(req, res) {
       var emailSubject = 'Validate you\'re subscription';
       var emailMessage = 'Please validate you\'re ' +
       'node.js releases subscription. ' +
-      'http://node-releases.cloudapp.net/validate/' + token;
+      'http://releases.nodenica.com/validate/' + token;
       lib.sendEmail(req.body.email, emailSubject, emailMessage);
     }
     res.send(JSON.stringify(response));
